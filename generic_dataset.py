efe8e12b39e93a487e318c75e9227d484d267627
Taslim Dosunmu <tjdosunmu@gmail.com>
2020-08-13 13:31:04 -0400

    make generic dataset creator script more flexible

--- generic_dataset.py
+++ generic_dataset.py
@@ -1,55 +1,41 @@
 #! /usr/bin/env python3
-'''
-
-Split: 75 less, 15 not, 10 more
-
-existing dirs
-less_distorted
-    training
-        classes
-    validation
-        classes
-
-more_distorted
-    training
-        classes
-    validation
-        classes
-
-not_distorted
-    training
-        classes
-    validation
-        classes
-
-result:
-generic
-    training
-        classes
-            75 less
-            15 not
-            10 more
-    validation
-        classes
-            75 less
-            15 not
-            10 more
-'''
 
 import os
 import re
 
+'''
+Note that the sample dir is expected to have the different classes (e.g.
+not_distorted, less_distorted, more_distorted)
+'''
+
 if __name__ == '__main__':
-    classes = {
-        'not_distorted': 0.15,
-        'less_distorted': 0.75,
-        'more_distorted': 0.10
-    }
+    if len(sys.argv) < 3:
+        print('Invalid command usage:')
+        print('Proper usage (replace <> fields with values):')
+        print('\tgeneric_dataset <sample directory> <output directory>')
+        print('  Or')
+        print('\tgeneric_dataset <sample directory> <output directory> <sample class>=<ratio of existing samples>')
+        sys.exit(1)
+
+    samples_dir = sys.argv[1]
+    output = sys.argv[2]
+    if len(sys.argv) == 3:
+        classes = {
+            'not_distorted': 0.15,
+            'less_distorted': 0.75,
+            'more_distorted': 0.10
+        }
+    else:
+        classes = {}
+
+        for sample_opt in sys.argv[3:]:
+            sample = sample_opt.split('=')
+            if len(sample) != 2:
+                print(f'invalid option: {sample_opt}. Sample options must look like: <sample class>=<float ratio>, e.g. not_distorted=0.15')
+            classes[sample[0]] = float(sample[1])
 
     for distort_class, ratio in classes.items():
-        # img_dir is where the image file dirs were located on my system
-        img_dir = 'ssa-data/{}/'.format(distort_class)
-        target_dir = 'generic-data/'
+        img_dir = f'{samples_dir}/{distort_class}/'
 
         TRAINING = lambda img_dir: img_dir + 'training'
         VALIDATION = lambda img_dir: img_dir + 'validation'
@@ -61,7 +47,7 @@ if __name__ == '__main__':
 
                 for i in range(round(len(samples) * ratio)):
                     copy_name = '{}.{}.png'.format(samples[i][:-4],distort_class)
-                    copy_target = '{}/{}/{}'.format(SUBDIR(target_dir), sat, copy_name)
+                    copy_target = '{}/{}/{}'.format(SUBDIR(output), sat, copy_name)
                     original = sat_dir + '/' + samples[i]
 
                     # Because the imgage filenames contained parantheses at the time, they need to be escaped
